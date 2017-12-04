31698820348666a82fa99dbdf0312a706721e809
Mihal Malostanidis <qm3ster@gmail.com>
2017-12-04 04:14:31 +0200

    Forgive me father, what the heck was that? A tuna?

--- template/micro.config.js
+++ template/micro.config.js
@@ -3,9 +3,7 @@ const {send} = require('micro')
 const {getUser, getUsers} = require('./api/')
 const nuxt = require('./nuxt.index.js')
 
-module.exports = async (req, res) => {
-  await dispatch()
-    .dispatch('/api/users/:id', ['GET'], async (req, res, {params, query}) => send(res, 200, await getUser(params.id)))
-    .dispatch('/api/users', ['GET'], async (req, res) => send(res, 200, await getUsers()))
-    .dispatch('*', ['GET'], (req, res) => nuxt.render(req, res))(req, res)
-}
+module.exports = dispatch()
+  .dispatch('/api/users/:id', ['GET'], async (req, res, {params, query}) => send(res, 200, await getUser(params.id)))
+  .dispatch('/api/users', ['GET'], async (req, res) => send(res, 200, await getUsers()))
+  .dispatch('*', ['GET'], (req, res) => nuxt.render(req, res))
